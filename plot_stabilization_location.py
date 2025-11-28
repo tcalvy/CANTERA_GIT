c16d496b4cce821d9338476779131fdeef5448e3
tcalvy <thomas.calvy1@gmail.com>
2025-11-28 18:55:41 +0100

    This commit include a functional version of the code for both temperature and massflow rate.

--- plot_stabilization_location.py
+++ plot_stabilization_location.py
@@ -35,6 +35,7 @@ with open("plot_config.yaml", "r") as f:
 # CONFIG
 # ----------------------------------------------------------
 folder = config['path']['output_folder'] 
+type = config['type']
 
 XOH_figsizex = config['XOH_PLOT']['figsizex']
 XOH_figsizey = config['XOH_PLOT']['figsizey']
@@ -58,88 +59,174 @@ MAX_ymax = config['MAX_PLOT']['ymax']
 output_folder = folder + "/PLOTS"
 
 
-# Regex to extract mdot_fuel and mdot_oxy from filename
-pattern = re.compile(r'diffusion_flame_H2AIR_(\d+)p(\d+)_(\d+)p(\d+)\.csv')
 
-ratios = []
-max_locations = []
+#%%------------------------PLOT FOR DEBIT CONFIGURATION------------------------------------------------------
+if type == 'debit' :
+      # Regex to extract mdot_fuel and mdot_oxy from filename
+      pattern = re.compile(r'diffusion_flame_H2AIR_DEBIT_(\d+)p(\d+)_(\d+)p(\d+)\.csv')
+      ratios = []
+      max_locations = []
 
-plt.figure(figsize=(XOH_figsizex,XOH_figsizey))
-
-# ----------------------------------------------------------
-# PROCESS FILES
-# ----------------------------------------------------------
-for filepath in glob.glob(os.path.join(folder, "diffusion_flame_H2AIR_*.csv")):
-    filename = os.path.basename(filepath)
-    match = pattern.match(filename)
-    if not match:
-        continue
-
-
-
-    # Convert '0p00127' to 0.00127
-    mdot_fuel = float(f"{match.group(1)}.{match.group(2)}")
-    mdot_oxy  = float(f"{match.group(3)}.{match.group(4)}")
-    
-    ratio = mdot_fuel / mdot_oxy
-
-    # Load into DataFrame
-    df = pd.read_csv(filepath, comment='#')
-    
-    # Extract numpy arrays
-    grid = df["grid"]
-    X_OH = df["X_OH"]
-
-    plt.plot(grid, X_OH, label='$\dot{m}_{fuel} = $'+ str(mdot_fuel))
-
-    # Compute index of maximum
-    idx_max = np.argmax(X_OH)
-    grid_at_max = grid[idx_max]
-
-    ratios.append(ratio)
-    max_locations.append(grid_at_max)
-
-# Convert to arrays & sort by mass-flow-ratio
-ratios = np.array(ratios)
-max_locations = np.array(max_locations)
-order = np.argsort(ratios)
-
-ratios = ratios[order]
-max_locations = max_locations[order]
-
-
-if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+      plt.figure(figsize=(XOH_figsizex,XOH_figsizey))
+      # ----------------------------------------------------------
+      # PROCESS FILES
+      # ----------------------------------------------------------
+      for filepath in glob.glob(os.path.join(folder, "diffusion_flame_H2AIR_DEBIT_*.csv")):
+            filename = os.path.basename(filepath)
+            match = pattern.match(filename)
+            if not match:
+                  continue
 
 
 
+            # Convert '0p00127' to 0.00127
+            mdot_fuel = float(f"{match.group(1)}.{match.group(2)}")
+            mdot_oxy  = float(f"{match.group(3)}.{match.group(4)}")
+            
+            ratio = mdot_fuel / mdot_oxy
 
-plt.xlabel("Grid location ($m$)")
-if XOH_xlimactiv == 'yes' :
-      plt.xlim([XOH_xmin,XOH_xmax])
-if XOH_ylimactiv == 'yes' :
-      plt.xlim([XOH_ymin,XOH_ymax])
-plt.ylabel("$X_{OH}$")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.savefig(os.path.join(output_folder, "XCO_grid.png"), dpi=300)
-plt.show()
+            # Load into DataFrame
+            df = pd.read_csv(filepath, comment='#')
+            
+            # Extract numpy arrays
+            grid = df["grid"]
+            X_OH = df["X_OH"]
 
-# ----------------------------------------------------------
-# PLOT RESULT
-# ----------------------------------------------------------
+            plt.plot(grid, X_OH, label='$\dot{m}_{fuel} = $'+ str(mdot_fuel))
 
-plt.figure(figsize=(MAX_figsizex,MAX_figsizey))
-plt.plot(ratios, max_locations, marker="+")
-if MAX_xlimactiv == 'yes' :
-      plt.xlim([MAX_xmin,MAX_xmax])
-if MAX_ylimactiv == 'yes' :
-      plt.xlim([MAX_ymin,MAX_ymax])
-plt.xlabel("Mass-flow-rate ratio  mdot_fuel / mdot_oxy")
-plt.ylabel("Grid location of max(X_OH)")
-plt.title("Position of Maximum OH vs Mass Flow Rate Ratio")
-plt.savefig(os.path.join(output_folder, "max_XCO_ratio_mf.png"), dpi=300)
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+            # Compute index of maximum
+            idx_max = np.argmax(X_OH)
+            grid_at_max = grid[idx_max]
+
+            ratios.append(ratio)
+            max_locations.append(grid_at_max)
+
+      # Convert to arrays & sort by mass-flow-ratio
+      ratios = np.array(ratios)
+      max_locations = np.array(max_locations)
+      order = np.argsort(ratios)
+
+      ratios = ratios[order]
+      max_locations = max_locations[order]
+
+
+      if not os.path.exists(output_folder):
+                  os.makedirs(output_folder)
+
+
+      plt.xlabel("Grid location ($m$)")
+      if XOH_xlimactiv == 'yes' :
+            plt.xlim([XOH_xmin,XOH_xmax])
+      if XOH_ylimactiv == 'yes' :
+            plt.xlim([XOH_ymin,XOH_ymax])
+      plt.ylabel("$X_{OH}$")
+      plt.legend()
+      plt.grid(True)
+      plt.tight_layout()
+      plt.savefig(os.path.join(output_folder, "XCO_grid_debit.png"), dpi=300)
+      plt.show()
+
+      # ----------------------------------------------------------
+      # PLOT RESULT
+      # ----------------------------------------------------------
+
+      plt.figure(figsize=(MAX_figsizex,MAX_figsizey))
+      plt.plot(ratios, max_locations, marker="+")
+      if MAX_xlimactiv == 'yes' :
+            plt.xlim([MAX_xmin,MAX_xmax])
+      if MAX_ylimactiv == 'yes' :
+            plt.xlim([MAX_ymin,MAX_ymax])
+      plt.xlabel("Mass-flow-rate ratio  mdot_fuel / mdot_oxy")
+      plt.ylabel("Grid location of max(X_OH)")
+      plt.savefig(os.path.join(output_folder, "max_XCO_ratio_debit.png"), dpi=300)
+      plt.grid(True)
+      plt.tight_layout()
+      plt.show()
+
+
+
+
+#%%------------------------PLOT FOR TEMPERATURE CONFIGURATION--------------------------------------------
+if type == 'temperature' :
+      # Regex to extract mdot_fuel and mdot_oxy from filename
+      pattern = re.compile(r'diffusion_flame_H2AIR_TEMP_(\d+)p(\d+)_(\d+)p(\d+)\.csv')
+      ratios = []
+      max_locations = []
+
+
+      plt.figure(figsize=(XOH_figsizex,XOH_figsizey))
+      # ----------------------------------------------------------
+      # PROCESS FILES
+      # ----------------------------------------------------------
+      for filepath in glob.glob(os.path.join(folder, "diffusion_flame_H2AIR_TEMP_*.csv")):
+            filename = os.path.basename(filepath)
+            match = pattern.match(filename)
+            if not match:
+                  continue
+                  
+
+
+
+            # Convert '0p00127' to 0.00127
+            t_fuel = float(f"{match.group(1)}.{match.group(2)}")
+            t_oxi  = float(f"{match.group(3)}.{match.group(4)}")
+            ratio = t_fuel / t_oxi
+
+            # Load into DataFrame
+            df = pd.read_csv(filepath, comment='#')
+            
+            # Extract numpy arrays
+            grid = df["grid"]
+            X_OH = df["X_OH"]
+
+            plt.plot(grid, X_OH, label='$T_{oxidizer} = $'+ str(t_oxi))
+
+            # Compute index of maximum
+            idx_max = np.argmax(X_OH)
+            grid_at_max = grid[idx_max]
+
+            ratios.append(ratio)
+            max_locations.append(grid_at_max)
+
+      # Convert to arrays & sort by mass-flow-ratio
+      ratios = np.array(ratios)
+      max_locations = np.array(max_locations)
+      order = np.argsort(ratios)
+
+      ratios = ratios[order]
+      max_locations = max_locations[order]
+
+
+      if not os.path.exists(output_folder):
+                  os.makedirs(output_folder)
+
+
+      plt.xlabel("Grid location ($m$)")
+      if XOH_xlimactiv == 'yes' :
+            plt.xlim([XOH_xmin,XOH_xmax])
+      if XOH_ylimactiv == 'yes' :
+            plt.xlim([XOH_ymin,XOH_ymax])
+      plt.ylabel("$X_{OH}$")
+      plt.legend()
+      plt.grid(True)
+      plt.tight_layout()
+      plt.savefig(os.path.join(output_folder, "XCO_grid_temp.png"), dpi=300)
+      plt.show()
+
+      # ----------------------------------------------------------
+      # PLOT RESULT
+      # ----------------------------------------------------------
+
+      plt.figure(figsize=(MAX_figsizex,MAX_figsizey))
+      plt.plot(ratios, max_locations, marker="+")
+      if MAX_xlimactiv == 'yes' :
+            plt.xlim([MAX_xmin,MAX_xmax])
+      if MAX_ylimactiv == 'yes' :
+            plt.xlim([MAX_ymin,MAX_ymax])
+      plt.xlabel("Mass-flow-rate ratio  mdot_fuel / mdot_oxy")
+      plt.ylabel("Grid location of max(X_OH)")
+      plt.savefig(os.path.join(output_folder, "max_XCO_ratio_temperature.png"), dpi=300)
+      plt.grid(True)
+      plt.tight_layout()
+      plt.show()
+
--- serial_CFF_yaml_proven.py
+++ serial_CFF_yaml_proven.py
@@ -27,13 +27,13 @@ if type == 'debit' :
 
     loglevel = 1  # amount of diagnostic output (0 to 5)
 
-    min_fuel = config['d_debits']['min_fuel']
-    max_fuel = config['d_debits']['max_fuel']
-    nb_pts_fuel = config['d_debits']['nb_points_fuel']
+    min_d_fuel = config['d_debits']['min_fuel']
+    max_d_fuel = config['d_debits']['max_fuel']
+    nb_pts_d_fuel = config['d_debits']['nb_points_fuel']
 
-    min_ox = config['d_debits']['min_oxidizer']
-    max_ox = config['d_debits']['max_oxidizer']
-    nb_pts_ox = config['d_debits']['nb_points_oxidizer']
+    min_d_ox = config['d_debits']['min_oxidizer']
+    max_d_ox = config['d_debits']['max_oxidizer']
+    nb_pts_d_ox = config['d_debits']['nb_points_oxidizer']
 
  
 
@@ -42,8 +42,8 @@ if type == 'debit' :
     gas = ct.Solution('gri30.yaml')
     gas.TP = gas.T, p
 
-    list_mdot_o = np.linspace(min_ox, max_ox, nb_pts_ox)    # kg/m^2/s
-    list_mdot_f = np.linspace(min_fuel, max_fuel, nb_pts_fuel)  # kg/m^2/s
+    list_mdot_o = np.linspace(min_d_ox, max_d_ox, nb_pts_d_ox)    # kg/m^2/s
+    list_mdot_f = np.linspace(min_d_fuel, max_d_fuel, nb_pts_d_fuel)  # kg/m^2/s
 
     for mdot_o in list_mdot_o :
         for mdot_f in list_mdot_f : 
@@ -77,7 +77,7 @@ if type == 'debit' :
             if not os.path.exists(folder_name):
                 os.makedirs(folder_name)
 
-            csv_path = (f"{folder_name}/diffusion_flame_H2AIR_{mdot_f:.5f}_{mdot_o:.5f}"
+            csv_path = (f"{folder_name}/diffusion_flame_H2AIR_DEBIT_{mdot_f:.5f}_{mdot_o:.5f}"
             .replace(".", "p")
             + ".csv"
     )
@@ -95,3 +95,80 @@ if type == 'debit' :
             f.show_stats()
             no_rad = f.to_array()
 
+#%%CONFIG TEMPERATURE INCREMENTATION
+if type == 'temperature' : 
+    p = config['t_properties']['pressure']
+    d_fuel = config['t_properties']['debit_fuel']
+    d_ox = config['t_properties']['debit_oxidizer']
+
+    loglevel = 1  # amount of diagnostic output (0 to 5)
+
+    min_t_fuel = config['t_temperatures']['min_fuel']
+    max_t_fuel = config['t_temperatures']['max_fuel']
+    nb_pts_t_fuel = config['t_temperatures']['nb_points_fuel']
+
+    min_t_ox = config['t_temperatures']['min_oxidizer']
+    max_t_ox = config['t_temperatures']['max_oxidizer']
+    nb_pts_t_ox = config['t_temperatures']['nb_points_oxidizer']
+
+ 
+
+    # Create the gas object used to evaluate all thermodynamic, kinetic, and
+    # transport properties.
+    gas = ct.Solution('gri30.yaml')
+ 
+
+    list_t_o = np.linspace(min_t_ox, max_t_ox, nb_pts_t_ox)    # K
+    list_t_f = np.linspace(min_t_fuel, max_t_fuel, nb_pts_t_fuel)  # K
+
+    for temp_o in list_t_o :
+        for temp_f in list_t_f : 
+
+            gas.TP = gas.T, p
+
+            # Create an object representing the counterflow flame configuration,
+            # which consists of a fuel inlet on the left, the flow in the middle,
+            # and the oxidizer inlet on the right.
+            f = ct.CounterflowDiffusionFlame(gas, width=width)
+
+            # Set the state of the two inlets
+            f.fuel_inlet.mdot = d_fuel
+            f.fuel_inlet.X = comp_f
+            f.fuel_inlet.T = temp_f
+
+            f.oxidizer_inlet.mdot = d_ox
+            f.oxidizer_inlet.X = comp_o
+            f.oxidizer_inlet.T = temp_o
+
+
+            f.set_refine_criteria(ratio=4, slope=0.2, curve=0.3, prune=0.04)
+
+            f.solve(loglevel, auto=True)
+
+            # --- Write important info into the first line of the CSV ---
+            header = (
+                f"mdot_f={d_fuel}, mdot_o={d_ox}, "
+                f"comp_f={comp_f}, comp_o={comp_o}, "
+                f"tin_f={temp_f}, tin_o={temp_o}"
+            )
+
+            if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
+
+            csv_path = (f"{folder_name}/diffusion_flame_H2AIR_TEMP_{temp_f:04.4f}_{temp_o:04.4f}"
+            .replace(".", "p")
+            + ".csv"
+    )
+            # Step 1: let Cantera create the CSV
+            f.save(csv_path, basis="mole", overwrite=True)
+
+            # Step 2: prepend your header
+            with open(csv_path, "r") as f_read:
+                original = f_read.read()
+
+            with open(csv_path, "w") as f_write:
+                f_write.write("# " + header + "\n" + original)
+
+            # Step 3: continue processing
+            f.show_stats()
+            no_rad = f.to_array()
